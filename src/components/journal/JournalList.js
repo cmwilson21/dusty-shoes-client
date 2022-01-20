bf7aba16fcfe704e9fa34b9b40311b9c2a8d7337
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-01-19 23:34:27 -0600

    removed extra code

--- src/components/journal/JournalList.js
+++ src/components/journal/JournalList.js
@@ -1,58 +1,55 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { NavLink, useHistory } from 'react-router-dom'
-import { loadTrips } from '../../actions/trips'
-import { Grid, Card, CardContent } from '@mui/material'
-import CheckIcon from '@mui/icons-material/Check';
-import { imgDetails } from '../../Globals'
-
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useHistory } from "react-router-dom";
+import { loadTrips } from "../../actions/trips";
+import { Grid, Card, CardContent } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { imgDetails } from "../../Globals";
 
 const JournalList = () => {
-  const trips = useSelector(state => state.trips)
-  const loggedIn = useSelector(state => state.sessions.loggedIn);
-  const currentUser = useSelector(state => state.sessions.currentUser)
-  const dispatch = useDispatch()
+  const trips = useSelector((state) => state.trips);
+  const loggedIn = useSelector((state) => state.sessions.loggedIn);
+  const currentUser = useSelector((state) => state.sessions.currentUser);
+  const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
-    // clearErrors();
     if (loggedIn) {
-      dispatch(loadTrips(localStorage.getItem('jwt'), currentUser))
-      // console.log("trips list currentUser", currentUser)
+      dispatch(loadTrips(localStorage.getItem("jwt"), currentUser));
     } else {
-      history.push("/login")
+      history.push("/login");
     }
-  }, [loggedIn, currentUser, dispatch, history])
-
-  // console.log("trips", trips)
-
+  }, [loggedIn, currentUser, dispatch, history]);
 
   const tripsLi = trips.map((trip, index) => (
     <div key={index} align="center">
       {trip.been_there === true ? (
         <Grid item md={3}>
           <Card sx={{ maxWidth: 345 }} style={{ marginTop: "12px" }}>
-            <CardContent><NavLink to={`/trips/${trip.id}`}>{trip.city}, {trip.country}</NavLink>
+            <CardContent>
+              <NavLink to={`/trips/${trip.id}`}>
+                {trip.city}, {trip.country}
+              </NavLink>
               <br />
-              <img style={{ ...imgDetails }} src={trip.image_url} alt="travel" />
+              <img
+                style={{ ...imgDetails }}
+                src={trip.image_url}
+                alt="travel"
+              />
             </CardContent>
             <CheckIcon />
           </Card>
         </Grid>
-      ) : (
-        null
-      )}
+      ) : null}
     </div>
-  ))
-
+  ));
 
   return (
     <div>
       <h4>Go somewhere and click Been There to see it in your journal.</h4>
       {tripsLi}
     </div>
-  )
-}
+  );
+};
 
-export default JournalList
+export default JournalList;
--- src/components/sessions/Signup.js
+++ src/components/sessions/Signup.js
@@ -1,44 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { signup } from '../../actions/sessions'
-import { useHistory } from 'react-router-dom'
-import { Box, TextField, Button } from '@mui/material'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { signup } from "../../actions/sessions";
+import { useHistory } from "react-router-dom";
+import { Box, TextField, Button } from "@mui/material";
 
 const Signup = () => {
   const [state, setState] = useState({
     first_name: "",
     last_name: "",
     email: "",
-    password: ""
-  })
-  const dispatch = useDispatch()
-  const history = useHistory()
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setState({
       ...state,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log('state', state)
-    dispatch(signup(state, history))
-  }
+    dispatch(signup(state, history));
+  };
 
   useEffect(() => {
     return () => {
-      dispatch({ type: "CLEAR_ERRORS" })
-    }
-  }, [dispatch])
-
+      dispatch({ type: "CLEAR_ERRORS" });
+    };
+  }, [dispatch]);
 
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
@@ -46,16 +44,58 @@ const Signup = () => {
     >
       <div>
         <h3>Please Signup Below</h3>
-        <TextField required label="Required" variant="filled" id="outlined-required" name="first_name" value={state.first_name} placeholder="First Name" autoFocus={true} helperText="First Name" onChange={handleChange} />
-        <TextField required label="Required" variant="filled" name="last_name" id="filled-required" value={state.last_name} placeholder="Last Name" helperText="Last Name" onChange={handleChange} />
+        <TextField
+          required
+          label="Required"
+          variant="filled"
+          id="outlined-required"
+          name="first_name"
+          value={state.first_name}
+          placeholder="First Name"
+          autoFocus={true}
+          helperText="First Name"
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          label="Required"
+          variant="filled"
+          name="last_name"
+          id="filled-required"
+          value={state.last_name}
+          placeholder="Last Name"
+          helperText="Last Name"
+          onChange={handleChange}
+        />
         <br />
-        <TextField required label="Required" variant="filled" name="email" id="email" value={state.email} placeholder="Enter your email" helperText="Email" onChange={handleChange} />
-        <TextField required label="Required" variant="filled" type="password" name="password" id="password" value={state.password} placeholder="Create a password" helperText="Password" onChange={handleChange} />
+        <TextField
+          required
+          label="Required"
+          variant="filled"
+          name="email"
+          id="email"
+          value={state.email}
+          placeholder="Enter your email"
+          helperText="Email"
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          label="Required"
+          variant="filled"
+          type="password"
+          name="password"
+          id="password"
+          value={state.password}
+          placeholder="Create a password"
+          helperText="Password"
+          onChange={handleChange}
+        />
         <br />
         <Button type="submit">Signup</Button>
       </div>
     </Box>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
