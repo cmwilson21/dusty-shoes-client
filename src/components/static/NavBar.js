af2734dc72acc405c7ee478ac2c4a2ea3022d1d3
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-01-19 23:36:14 -0600

    cleaned up more comments

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -1,49 +1,89 @@
-import React from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { logout } from '../../actions/sessions'
-
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../actions/sessions";
 
 const NavBar = () => {
-  const loggedIn = useSelector(state => state.sessions.loggedIn)
-  const currentUser = useSelector(state => state.sessions.currentUser)
+  const loggedIn = useSelector((state) => state.sessions.loggedIn);
+  const currentUser = useSelector((state) => state.sessions.currentUser);
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const logoutClick = e => {
+  const logoutClick = (e) => {
     e.preventDefault();
-    dispatch(logout())
-    history.push('/')
-  }
-
-  // console.log("in", loggedIn)
-
+    dispatch(logout());
+    history.push("/");
+  };
 
   if (loggedIn) {
-    const firstName = `${currentUser.first_name}`
+    const firstName = `${currentUser.first_name}`;
     return (
       <div>
         <ul className="nav-ul">
-          <li className="nav-li"><NavLink className="navbar_link" to="/">Home</NavLink></li>
-          <li className="nav-li"><NavLink className="navbar_link" to="/trips">Trips</NavLink></li>
-          <li className="nav-li"><NavLink className="navbar_link" to="/trips/new">Add A Trip</NavLink></li>
-          <li className="nav-li"><NavLink className="navbar_link" to="/journal">Travel Journal</NavLink></li>
-          <li className="nav-li"><NavLink className="navbar_link" to="/logout" onClick={logoutClick}>Logout</NavLink></li>
-          <NavLink style={{ textDecoration: "none" }} to="/trips/new"><p style={{ color: "white", textAlign: "right", marginRight: "55px" }}>Where to next, {firstName}?</p></NavLink>
+          <li className="nav-li">
+            <NavLink className="navbar_link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink className="navbar_link" to="/trips">
+              Trips
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink className="navbar_link" to="/trips/new">
+              Add A Trip
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink className="navbar_link" to="/journal">
+              Travel Journal
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink className="navbar_link" to="/logout" onClick={logoutClick}>
+              Logout
+            </NavLink>
+          </li>
+          <NavLink style={{ textDecoration: "none" }} to="/trips/new">
+            <p
+              style={{
+                color: "white",
+                textAlign: "right",
+                marginRight: "55px",
+              }}
+            >
+              Where to next, {firstName}?
+            </p>
+          </NavLink>
         </ul>
       </div>
-    )
+    );
   }
   return (
     <div>
       <ul className="nav-ul">
-        <li className="nav-li"><NavLink className="navbar_link" to="/">Home</NavLink></li>
-        <li className="nav-li"><NavLink className="navbar_link" to="/login">Login</NavLink></li>
-        <li className="nav-li"><NavLink className="navbar_link" to="/signup">Signup</NavLink></li>
-        <p style={{ color: "white", textAlign: "right", marginRight: "55px" }}>Dusty Shoes</p>
+        <li className="nav-li">
+          <NavLink className="navbar_link" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink className="navbar_link" to="/login">
+            Login
+          </NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink className="navbar_link" to="/signup">
+            Signup
+          </NavLink>
+        </li>
+        <p style={{ color: "white", textAlign: "right", marginRight: "55px" }}>
+          Dusty Shoes
+        </p>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
